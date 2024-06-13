860496197c5fa237701e06ea6a4af7f591330106
MikiRedo <miki@MacBook-Air-de-Miquel.local>
2024-06-13 10:19:39 +0200

    preparing network config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,5 +1,11 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+const { vars } = require("hardhat/config");  
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");   //or process.env.KEY??
+
+const PRIVATE_KEY = vars.get("PRIVATE_KEY");    
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
@@ -9,10 +15,12 @@ module.exports = {
     cache: "./cache",
     artifacts: "./artifacts"
   },
+  //should we do that on mumbay???
   networks: {
-    hardhat: {
-      chainId: 1337
-    }
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`, //mumbay??
+      accounts: [PRIVATE_KEY],
+    },
   }
 };
 
