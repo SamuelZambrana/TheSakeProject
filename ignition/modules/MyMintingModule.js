ffbf718e5167115e9dbd03e97cd82bee8fab2253
MikiRedo <miki@MacBook-Air-de-Miquel.local>
2024-07-04 21:30:52 +0200

    new nft minted

--- ignition/modules/MyMintingModule.js
+++ ignition/modules/MyMintingModule.js
@@ -3,14 +3,14 @@ require("dotenv").config({ path: ".env" });
 const { ethers } = require("ethers");
 
 const MyMintingModule = buildModule("MyMintingModule", (m) => {
-    const contractAddress = process.env.CONTRACT_ADDRESS;
+    const contractAddress = process.env.NFT_CONTRACT_ADDRESS;
     const privateKey = process.env.PRIVATE_KEY;
     const wallet = new ethers.Wallet(privateKey);
     const ourWallet = wallet.address; // Dirección de la wallet asociada a la clave privada
 
     const MyNFT = m.contractAt("TheSakiNFTs", contractAddress);
 
-    const tokenUri = "QmdJ4MF8abq466wXCoq6Jek5LqoQLvPAhx1nbqvE3rCxfB";
+    const tokenUri = "QmY2JNUZprVtkXvRVdHaRZkCEfAhMfowQcb4vuBNsMaLWx";
 
     m.call(MyNFT, "safeMint", [ourWallet, tokenUri]);
     
